209a7cf2441721b8754f36d6fcad66276c7eba59
SamuelLabrador <sam.labrador@yahoo.com>
2019-04-27 12:20:12 -0700

    Updated gradle build and AndroidManifest.xml

--- build/app/generated/source/buildConfig/debug/com/example/fluttersnapchat/BuildConfig.java
+++ build/app/generated/source/buildConfig/debug/com/example/fluttersnapchat/BuildConfig.java
@@ -5,7 +5,7 @@ package com.example.fluttersnapchat;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.example.fluttersnapchat";
+  public static final String APPLICATION_ID = "com.citrushack.dietapp";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
