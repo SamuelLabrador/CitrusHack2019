6d9646aedc488b3b6b93c87ebd257dbdd74231e6
SamuelLabrador <sam.labrador@yahoo.com>
2019-04-28 03:33:37 -0700

    everythings ready for merge

--- lib/camera_page.dart
+++ lib/camera_page.dart
@@ -86,6 +86,7 @@ Future<Null> labelImage(FirebaseVisionImage image) async{
      for(var i = 0; i < labels.length; i++){
        if (labels[i].text.toLowerCase() == key.toString()) {
         var newEntry = reference.child('history').push();
+//        print(labels[i]);
         newEntry.update({
           "food" : key.toString(),
           "date" : new DateTime.now().toString(),
